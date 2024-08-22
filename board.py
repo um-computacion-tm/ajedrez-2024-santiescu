06d5386101f2e1d07b49fdefc9d388b69c3c56f3
Santiago <santiescud@gmail.com>
2024-08-21 23:55:28 -0300

    update board

--- board.py
+++ board.py
@@ -14,3 +14,17 @@ class Board:
 
     def get_piece(self, row, col):
         return self.__positions__ [row][col]
+    
+    def __str__(self):
+        board_str = ""
+        for row in self.__positions__:
+            for cell in row:
+                if cell is not None:
+                    board_str += str(cell)
+                else:
+                    board_str += " "
+            board_str += "\n"
+        return board_str
+
+
+  
