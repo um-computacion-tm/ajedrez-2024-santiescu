a8e549ee3083e0d4474533f22a4e9efee706d9d9
Santiago <santiescud@gmail.com>
2024-08-29 23:42:34 -0300

    upload rook

--- rook.py
+++ rook.py
@@ -4,8 +4,6 @@ class Rook(Piece):
     white_str = "♜"
     black_str = "♖"
 
-
-    
     def possible_positions_vd(self, row, col):
         possibles = []
         for next_row in range(row + 1, 8):
@@ -24,7 +22,6 @@ class Rook(Piece):
             possibles.append((next_row, col))
         return possibles
     
-
     def valid_positions(self,from_row,from_col,to_row,to_col,):
         possible_positions = (self.possible_positions_vd(from_row, from_col) + self.possible_positions_va(from_row, from_col) )
         return (to_row, to_col) in possible_positions
