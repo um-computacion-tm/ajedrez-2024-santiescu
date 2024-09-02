cd0d9804b94c100f1d076a7a01a5347fffe2b856
Santiago <santiescud@gmail.com>
2024-09-01 23:55:48 -0300

    Upload Test_rook

--- test_rook.py
+++ test_rook.py
@@ -1,15 +1,54 @@
 import unittest
-from pieces import Rook
+from rook import Rook
 from board import Board
+from pawn import Pawn
+
 class TestRook(unittest.TestCase):
-       
+
     def test_str(self):
-        board=Board()
-        rook=Rook('WHITE',board)
-        self.assertEqual(str(rook),'W')
-            
+        board = Board()
+        rook = Rook("WHITE", board)
+        self.assertEqual(
+            str(rook),
+            "♜",
+        )
+
     def test_move_vertical_desc(self):
-        board=Board()
-        rook=Rook('WHITE',board)
-        possibles=rook.possible_positions(4,1)
-        self.assertEqual(possibles,[(5,1),(6,1),(7,1)])
+        board = Board()
+        rook = Rook("WHITE", board)
+        possibles = rook.possible_positions_vd(4, 1)
+        self.assertEqual(
+            possibles,
+            [(5, 1), (6, 1), (7, 1)]
+        )
+
+    def test_move_vertical_asc(self):
+        board = Board()
+        rook = Rook("WHITE", board)
+        possibles = rook.possible_positions_va(4, 1)
+        self.assertEqual(
+            possibles,
+            [(3, 1), (2, 1), (1, 1), (0, 1)]
+        )
+
+    def test_move_vertical_desc_with_own_piece(self):
+        board = Board()
+        board.set_piece(6, 1, Pawn("WHITE", board))
+        rook = Rook("WHITE", board)
+        board.set_piece(4, 1, rook)
+        possibles = rook.possible_positions_vd(4, 1)
+        self.assertEqual(
+            possibles,
+            [(5, 1)]
+        )
+
+    def test_move_vertical_desc_with_not_own_piece(self):
+        board = Board()
+        board.set_piece(6, 1, Pawn("BLACK", board))
+        rook = Rook("WHITE", board)
+        board.set_piece(4, 1, rook)
+        possibles = rook.possible_positions_vd(4, 1)
+        self.assertEqual(
+            possibles,
+            [(5, 1), (6, 1)]
+        )
