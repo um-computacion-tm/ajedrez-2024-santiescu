67b642afab0633561c8c7ecca168db0153529d3d
Santiago <santiescud@gmail.com>
2024-09-06 00:22:38 -0300

    Update chess & TestBoard

--- chess.py
+++ chess.py
@@ -1,5 +1,6 @@
 from board import Board
-from exceptions import InvalidMove
+from exceptions import InvalidMove, InvalidTurn, EmptyPosition
+
 class Chess:
     def __init__(self):
         self.__board__ = Board()
@@ -15,11 +16,17 @@ class Chess:
         to_row,
         to_col,
     ):
-        # validate coords
+        
         piece = self.__board__.get_piece(from_row, from_col)
-        if piece.valid_positions(from_row, from_col, to_row, to_col):
+        if not piece:
+            raise EmptyPosition()
+        if not piece.get_color() == self.__turn__:
+            raise InvalidTurn()
+        if not piece.valid_positions(from_row, from_col, to_row, to_col):
             raise InvalidMove()
+        self.__board__.move(from_row, from_col, to_row, to_col)
         self.change_turn()
+
 
     def turn(self):
         return self.__turn__
--- test_board.py
+++ test_board.py
@@ -1,5 +1,7 @@
 import unittest
 from board import Board
+from rook import Rook
+from exceptions import OutOfBoard
 
 class TestBoard(unittest.TestCase):
     def test_str_board(self):
@@ -16,4 +18,44 @@ class TestBoard(unittest.TestCase):
                 "        \n"
                 "♜      ♜\n"
             )
+        )
+    def test_move(self):
+        board = Board(for_test=True)
+        rook = Rook(color='BLACK', board=board)
+        board.set_piece(0, 0, rook)
+
+        board.move(
+            from_row=0,
+            from_col=0,
+            to_row=0,
+            to_col=1,
+        )
+
+        self.assertIsInstance(
+            board.get_piece(0, 1),
+            Rook,
+        )
+        self.assertEqual(
+            str(board),
+            (
+                " ♖      \n"
+                "        \n"
+                "        \n"
+                "        \n"
+                "        \n"
+                "        \n"
+                "        \n"
+                "        \n"
+            )
+        )
+
+    def test_get_piece_out_of_range(self):
+        board = Board(for_test=True)
+
+        with self.assertRaises(OutOfBoard) as exc:
+            board.get_piece(10, 10)
+
+        self.assertEqual(
+            exc.exception.message,
+            "La posicion indicada se encuentra fuera del tablero"
         )
