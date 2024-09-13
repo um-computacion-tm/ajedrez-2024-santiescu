7a0c1a53cb90e0751f27832e37f7ecffb4fcc0d3
Santiago <santiescud@gmail.com>
2024-09-12 23:51:46 -0300

    Add knight

--- knight.py
+++ knight.py
@@ -1,22 +1,4 @@
-class Piece:
-    def __init__(self, color, board):
-        self.__color__ = color
-        self.__board__ = board
-
-    def __str__(self):
-        if self.__color__ == "WHITE":
-            return self.white_str
-        else:
-            return self.black_str
-        
-class Pawn(Piece):
-    def __init__(self,color,board):
-        super().__init__(color,board)
-    
-    def __str__(self):
-        if self.__color__=="WHITE":
-            return "♟"
-        else: return "♙"
+from pieces import Piece
 
 class Knight(Piece):
     def __init__(self, color, board):
