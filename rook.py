d99f0c2cfd1c80fc4ce9ac84329f7bf3bec91235
Santiago <santiescud@gmail.com>
2024-09-13 23:56:43 -0300

    update rook.py

--- rook.py
+++ rook.py
@@ -25,3 +25,14 @@ class Rook(Piece):
     def valid_positions(self,from_row,from_col,to_row,to_col,):
         possible_positions = (self.possible_positions_vd(from_row, from_col) + self.possible_positions_va(from_row, from_col) )
         return (to_row, to_col) in possible_positions
+    
+    def possible_positions_hr(self,row,col):
+        possibles=[]
+        for next_col in range(col + 1, 8):
+            other_piece = self.__board__.get_piece(row,next_col)
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((row,next_col))  
+                break  
+            else:possibles.append((row,next_col))
+        return possibles
