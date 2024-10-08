f616a83043fd812d061c0cce8d57b5a0b191f239
Santiago <santiescud@gmail.com>
2024-10-07 23:54:35 -0300

    Fix board

--- board.py
+++ board.py
@@ -10,7 +10,6 @@ class Board:
             for _ in range(8):
                 col.append(None)
             self.__positions__.append(col)
-
         if not for_test:
             self.rook_positions()
             self.pawn_positions()
@@ -22,23 +21,33 @@ class Board:
                 if cell is not None:
                     board_str += str(cell) + " "
                 else:
-                    board_str += ". "  # Cambié a punto para representar celdas vacías
+                    board_str += ". " 
             board_str += "\n"
         return board_str.strip()
 
-    def validate_position(self, row, col):
-        if row < 0 or row >= 8:
-            raise RowOutOfBoard()
-        if col < 0 or col >= 8:
-            raise ColumnOutOfBoard()
+#    def validate_position(self, row, col):
+#        if row < 0 or row >= 8:
+#            raise RowOutOfBoard()
+#        if col < 0 or col >= 8:
+#            raise ColumnOutOfBoard()
 
-    def get_piece(self, row, col):
-        self.validate_position(row, col)
-        return self.__positions__[row][col]
+    def get_piece(self,row,col):
+        if not 0 <= row < 8 and not 0 <= col < 8:
+            raise OutOfBoard()
+        elif not 0 <= row < 8:
+            raise RowOutOfBoard()
+        elif not 0 <= col < 8:
+            raise ColumnOutOfBoard()
+        else: return self.__positions__[row][col]
     
     def set_piece(self, row, col, piece):
-        self.validate_position(row, col)
-        self.__positions__[row][col] = piece
+        if not (0 <= row < 8 or 0 <= col < 8):
+            raise OutOfBoard()
+        elif not 0 <= row < 8:
+            raise RowOutOfBoard()
+        elif not 0 <= col < 8:
+            raise ColumnOutOfBoard()
+        else: self.__positions__[row][col] = piece
     
     def rook_positions(self): 
         self.__positions__[0][0] = Rook("BLACK", self)
