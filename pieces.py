d8d4e31df4ae770d867970e96797a9eb1963e537
Santiago <santiescud@gmail.com>
2024-10-11 23:47:25 -0300

    Update pieces

--- pieces.py
+++ pieces.py
@@ -9,6 +9,16 @@ class Piece:
         else:
             return self.black_str
         
+    def get_color(self):
+        return self.__color__
+
+    
+    def is_row_col_in_valid_positions(self,to_row,to_col,possible_positions):
+        if (to_row,to_col) in possible_positions:
+            return True
+        else: False
+        
+            
 class Pawn(Piece):
     def __init__(self,color,board):
         super().__init__(color,board)
@@ -21,11 +31,9 @@ class Pawn(Piece):
 class Knight(Piece):
     def __init__(self, color, board):
         super().__init__(color, board)
-        self.white_str = "♘"  # Símbolo para el caballo blanco
-        self.black_str = "♞"  # Símbolo para el caballo negro
+        self.white_str = "♘"  
+        self.black_str = "♞"  
 
     def is_valid_piece_move(self, board, from_row, from_col, to_row, to_col):
-        """Verifica si el movimiento es válido para el caballo."""
-        # El caballo se mueve en forma de "L"
         return (abs(from_row - to_row) == 2 and abs(from_col - to_col) == 1) or \
                (abs(from_row - to_row) == 1 and abs(from_col - to_col) == 2)
